97fdfca230eac30993e012b2fecc6b4cc6e69d86
Tareq El-Ali <elalitareq@gmail.com>
2023-02-15 21:07:41 +0200

    step_4

---
+++ step_3.php
@@ -0,0 +1,9 @@
+<?php
+$name="tareq";
+$email="elalitareq@gmail.com";
+echo "Hello, my name is $name and my email is $email";
+$height=10;
+$width=12;
+$area = $width * $height;
+echo "<br/>The area is $area";
+?>
---
+++ step_4.php
@@ -0,0 +1,9 @@
+<?php
+$name="tareq";
+$email="elalitareq@gmai                                                                                             l.com";
+echo "Hello, my name is $name and my email is $email";
+$height=10;
+$width=12;
+$area = $width * $height;
+echo "$area";
+?>
