00eeb2081a7f7fb7a159c08c7739f5d016ba7972
Tareq El-Ali <elalitareq@gmail.com>
2023-02-16 15:38:01 +0200

    i dont get step 7

--- Basics 2/step_7.php
+++ Basics 2/step_7.php
@@ -1,7 +1,6 @@
 <?php
 $string = "I love Python, but I also love Python";
 $newString = str_replace("Python", "PHP", $string);
-$newString = str_replace("PHP", " PHP", $newString);
 echo $newString; 
 
 ?>
